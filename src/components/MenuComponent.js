6f43de88a023005bb993a65fbd72289c936b93ef
tehv1007 <hanquoclythu@gmail.com>
2022-06-09 19:22:01 +0700

    change Menu component

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -7,22 +7,6 @@ class Menu extends Component {
         super(props);
     }
 
-    renderDish(dish) {
-        if (dish != null) {
-            return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            )
-        }else {
-            return (<div></div>)
-        }
-    }
-
     render() {
 
         const menu = this.props.dishes.map((dish) => {
