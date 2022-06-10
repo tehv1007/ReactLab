caa72a3b8868cbcb1471dc9c69c00e395cd5a8a7
tehv1007 <hanquoclythu@gmail.com>
2022-06-10 12:16:25 +0700

    completed

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
@@ -20,11 +21,10 @@ class Main extends Component {
         let dish = dishes.find((dish) => dish.id === this.state.selectedDish);
         return (
             <div className="container">
-                <Navbar dark color="primary"> 
-                    <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-                </Navbar>
+                <Header/>
                 <Menu dishes={this.state.dishes} onDishSelect={(dishId) => this.onDishSelect(dishId)}/>
                 <Dishdetail dish={dish}/>
+                <Footer/>
             </div>
         )
     }
